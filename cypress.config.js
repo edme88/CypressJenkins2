d6b5a169b93dc6f804b4867f00346cc1ba6bdaeb
EXB04891 <agustina.aliciardi@darwoft.com>
2023-12-21 11:43:46 -0300

    Se agrego dependencia para tags

--- cypress.config.js
+++ cypress.config.js
@@ -11,9 +11,14 @@ module.exports = defineConfig({
     json: true,
     html: true,
   },
+  env: {
+    grepFilterSpecs: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("@cypress/grep/src/plugin")(config);
+      return config;
     },
   },
 });
